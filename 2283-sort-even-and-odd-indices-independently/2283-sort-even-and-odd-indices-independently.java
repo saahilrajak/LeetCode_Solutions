4497edba710b36802fc661067960c1678149b6d0
sahil rajak <110772316+saahilrajak@users.noreply.github.com>
2024-09-28 20:11:00 +0530

    Time: 2 ms (93.33%), Space: 44.6 MB (47.33%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
@@ -1,33 +1,38 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
     public int[] sortEvenOdd(int[] nums) {
-        ArrayList<Integer> evenIndices = new ArrayList<>();
-        ArrayList<Integer> oddIndices = new ArrayList<>();
-        // Separate even and odd indexed elements
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) {
-                evenIndices.add(nums[i]);
-            } else {
-                oddIndices.add(nums[i]);
+        int N = nums.length;
+        int[] oddArr = new int[N / 2 ];
+        int[] evenArr = new int[(N + 1) / 2];
+        int j = 0;
+        int k = 0;
+
+        for (int i=0; i<N; i++)
+        {
+            if (i % 2 == 0)
+            {
+                evenArr[j++] = nums[i];
+            }
+            else
+            {
+                oddArr[k++] = nums[i];
             }
         }
-        // Sort even indices in ascending order
-        Collections.sort(evenIndices);
-        // Sort odd indices in descending order
-        Collections.sort(oddIndices, Collections.reverseOrder());
-        // Place sorted elements back into original array
-        int evenIndex = 0, oddIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) {
-                nums[i] = evenIndices.get(evenIndex++);
-            } else {
-                nums[i] = oddIndices.get(oddIndex++);
+
+        Arrays.sort(evenArr);
+        Arrays.sort(oddArr);
+
+        int m = 0;
+        int n = oddArr.length - 1;
+        for (int i=0; i<nums.length; i++) 
+        {
+            if (i % 2 == 0) 
+            {
+                nums[i] = evenArr[m++];
+            } else 
+            {
+                nums[i] = oddArr[n--];
             }
         }
-        
         return nums;
     }
 }
