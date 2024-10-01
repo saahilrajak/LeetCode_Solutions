91e1c1da745d35d478dcca7db22b0f6e3ac2c640
sahil rajak <110772316+saahilrajak@users.noreply.github.com>
2024-10-01 18:55:13 +0530

    Time: 4 ms (98.97%), Space: 56.6 MB (52.06%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
@@ -1,38 +1,17 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        HashMap<Integer, Integer> remainderFrequency = new HashMap<>();
-
-        for (int val : arr) {
-            int rem = ((val % k) + k) % k;  // Handle negative remainders
-            int oldFrequency = remainderFrequency.getOrDefault(rem, 0);
-            remainderFrequency.put(rem, oldFrequency + 1);
+        int[] freq = new int[k];
+        
+        for (int num : arr) {
+            freq[((num % k) + k) % k]++;
         }
         
-        for (int val : arr) {
-            int rem = ((val % k) + k) % k;
-
-            if (rem == 0) { //If the remainder is 0, its frequency should be even
-                int frequency = remainderFrequency.get(rem);
-                if (frequency % 2 != 0) {
-                    return false;
-                }
-            } 
-            else if (2 * rem == k) {//the frequency of that remainder should be even
-                int frequency = remainderFrequency.get(rem);
-                if (frequency % 2 != 0) {
-                    return false;
-                }
-            }
-            else { //the frequency of rem and k - rem should match
-                int frequency = remainderFrequency.get(rem);
-                int otherFrequency = remainderFrequency.getOrDefault(k - rem, 0);
-                if (frequency != otherFrequency) {
-                    return false;
-                }
-            }
+        if (freq[0] % 2 != 0) return false;
+        
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) return false;
         }
+        
         return true;
     }
 }
