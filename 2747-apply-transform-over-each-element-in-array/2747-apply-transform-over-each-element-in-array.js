af0a27037b30542515e4c5abe5a906aaed619ec9
sahil rajak <110772316+saahilrajak@users.noreply.github.com>
2024-10-04 12:38:54 +0530

    Time: 38 ms (98.53%), Space: 49 MB (23.88%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -3,9 +3,9 @@
  * @param {Function} fn
  * @return {number[]}
  */
- 
+
 var map = function(arr, fn) {
-    const newArray = [];
+    let newArray = [];
     for (let i = 0; i < arr.length; i++) {
         newArray.push(fn(arr[i], i));
     }
