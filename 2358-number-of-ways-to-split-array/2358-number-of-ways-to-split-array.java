b3ffb7628257092f137301d26220403eb34bc21a
sahil rajak <110772316+saahilrajak@users.noreply.github.com>
2025-01-03 20:06:52 +0530

    Time: 3 ms (59.69%), Space: 60.7 MB (26.24%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -0,0 +1,28 @@
+//T.C : O(N)
+//S.C : O(N)
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        
+        // Calculate cumulative sum
+        long[] cumSum = new long[n];
+        cumSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            cumSum[i] = cumSum[i - 1] + nums[i];
+        }
+        
+        int split = 0;
+        
+        // Iterate and check conditions
+        for (int i = 0; i < n - 1; i++) {
+            long leftSum = cumSum[i];
+            long rightSum = cumSum[n - 1] - cumSum[i];
+            
+            if (leftSum >= rightSum) {
+                split++;
+            }
+        }
+        
+        return split;
+    }
+}
